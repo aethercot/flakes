f746264b8ae2fab638049926ee164c0815c9f2d1
aethercot <99132199+aethercot@users.noreply.github.com>
2023-06-10 14:40:17 +0200

    update hm git package, pls remember my credentials

--- home/packages/git/default.nix
+++ home/packages/git/default.nix
@@ -1,12 +1,24 @@
+{ pkgs, ... }:
+
 {
 	programs.git = {
 		enable = true;
+		package = pkgs.gitFull;
 
 		userName = "aethercot";
 		userEmail = "99132199+aethercot@users.noreply.github.com";
 
 		extraConfig = {
 			init.defaultBranch = "main";
+			credential.helper = "${pkgs.gitAndTools.gitFull}/bin/git-credential-libsecret";
 		};
+
+		ignores = [
+			"*~"
+			"*.swp"
+
+			"result"
+			"result-*"
+		];
 	};
 }
